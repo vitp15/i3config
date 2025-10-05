4685e9aca779c294f7f07cbfa3bc39ac4125a71b
vitp15 <vitp.work@gmail.com>
2025-10-05 18:00:40 +0300

    all scripts in its places

--- .config/polybar/scripts/hotspot-toggle.sh
+++ .config/polybar/scripts/hotspot-toggle.sh
@@ -3,6 +3,6 @@
 if nmcli -t -f NAME,TYPE connection show --active | grep -q "^Hotspot"; then
 	nmcli connection down Hotspot
 else
-    nmcli device wifi hotspot ifname wlo1 ssid "Hotspot 5G" password "12345678" band a channel 149
+    nmcli device wifi hotspot ifname wlo1 ssid "Internet" password "qwer2341" band a channel 149
 fi
 
