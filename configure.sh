ce8c7ab72022357fc7589975aadf5c340e4ec5ff
vitp15 <vadim2003plamadeala@gmail.com>
2025-10-05 18:31:21 +0300

    a part of configuration setted

--- configure.sh
+++ configure.sh
@@ -8,3 +8,18 @@ cp -r .tmux/* ~/.tmux
 mkdir ~/wallpapers -p
 cp -r wallpapers/* ~/wallpapers
 cp .tmux.conf ~/.tmux.conf
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt autoremove -y
+sudo apt install polybar -y
+sudo apt install feh -y
+sudo apt install lxappearance -y
+sudo apt install arandr -y
+sudo apt install pulseaudio pulseaudio-utils pavucontrol -y
+sudo apt install stalonetray -y
+sudo apt install brightnessctl -y
+sudo apt install xinput -y
+sudo apt install compton -y
+sudo apt install rofi -y
+
