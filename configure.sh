e8ba61f8c80b0a5621d2af1bd5d6de5e6c346f00
vitp15 <vadim2003plamadeala@gmail.com>
2025-10-15 18:54:03 +0300

    eth back

--- configure.sh
+++ configure.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#cp -r update.sh restart.sh shutdown.sh ~/.
-#cp -r .config/* ~/.config
-#mkdir ~/.fonts -p
-#cp -r .fonts/* ~/.fonts
-#mkdir ~/.tmux -p
-#cp -r .tmux/* ~/.tmux
-#mkdir ~/wallpapers -p
-#cp -r wallpapers/* ~/wallpapers
-#cp .tmux.conf ~/.tmux.conf
+cp -r update.sh restart.sh shutdown.sh ~/.
+cp -r .config/* ~/.config
+mkdir ~/.fonts -p
+cp -r .fonts/* ~/.fonts
+mkdir ~/.tmux -p
+cp -r .tmux/* ~/.tmux
+mkdir ~/wallpapers -p
+cp -r wallpapers/* ~/wallpapers
+cp .tmux.conf ~/.tmux.conf
 
 sudo apt update -y
 sudo apt upgrade -y
