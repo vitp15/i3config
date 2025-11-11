1596150d593c9e9b7be17edfe7a5b050ed3cf374
vitp15 <vitp.work@gmail.com>
2025-11-11 10:22:46 +0200

    hotspon name and password from .env

--- .config/polybar/scripts/hotspot-toggle.sh
+++ .config/polybar/scripts/hotspot-toggle.sh
@@ -2,9 +2,16 @@
 
 HOTSPOT_CONN=$(nmcli -t -f NAME,TYPE connection show --active | grep -E '^Hotspot' | cut -d: -f1)
 
+if [ -f .env ]; then
+  export $(grep -Ev '^$|^#' .env)
+else
+  echo ".env file not found"
+  exit 1
+fi
+
 if [ -n "$HOTSPOT_CONN" ]; then
     nmcli connection down "$HOTSPOT_CONN"
 else
-    nmcli device wifi hotspot ifname wlp3s0 ssid "Hotspot" password "12345678" band a channel 36
+    nmcli device wifi hotspot ifname wlp3s0 ssid "$HOTSPOT_NAME" password "$HOTSPOT_PASSWORD" band a channel 36
 fi
 
