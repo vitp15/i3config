ef4b085f2ab34a6dbe5d392a5f7201bf4125b7eb
vitp15 <vitp.work@gmail.com>
2025-11-11 10:27:57 +0200

    specific to work configuration

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,6 +4,12 @@ killall -q polybar
 killall -q stalonetray
 killall -q nm-applet
 
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 stalonetray --edge bottom --align right --icon-size 24 --geometry 1x1-0-0 &
 nm-applet >/dev/null 2>&1 &
-polybar mybar >> /tmp/polybar.log
+
+polybar --reload mybar >> /tmp/polybar.log &
+if [[ $(polybar -m | grep "HDMI-1: ") ]]; then
+	polybar --reload secondmonitorbar >> /tmp/polybar.log &
+fi
