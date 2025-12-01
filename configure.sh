8bf7e395c3f009995fa2fa8140b5dec08dda810b
vitp15 <vadim2003plamadeala@gmail.com>
2025-12-01 11:31:05 +0200

    copyq on local

--- configure.sh
+++ configure.sh
@@ -24,6 +24,7 @@ sudo apt install xinput -y
 sudo apt install compton -y
 sudo apt install rofi -y
 sudo apt install blueman -y
+sudo apt install copyq -y
 
 sudo apt install imagemagick -y
 sudo apt install i3lock -y
